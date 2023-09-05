15977059e37937ef613efd537c0423dfc20725b8
Elliot Lundin <firetail4373@gmail.com>
2023-09-05 08:32:48 +0200

    Prepared ConfigManager, settings file manager.

---
+++ header/ConfigManager.cpp
@@ -0,0 +1,8 @@
+#include "ConfigManager.h"
+
+void InitiateConfig()
+{
+	//First check if config file exists
+	//If it does, do nothing
+	//If it does'nt, create a config file.
+}
---
+++ header/ConfigManager.h
@@ -0,0 +1,4 @@
+#pragma once
+//Setup config for settings
+
+void InitiateConfig();
