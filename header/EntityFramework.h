79bcf9b651240d0193de820990cc9c61e79813a2
Niclas Andersson <niclas-andersson@outlook.com>
2023-09-06 14:30:13 +0200

    Update EntityFramework.h

--- header/EntityFramework.h
+++ header/EntityFramework.h
@@ -14,14 +14,37 @@ AddComponent(), templated function that adds some component (struct with some da
 Do we want this registry to be an unordered map? Since technically the "Entity" indicator itself could be the "key"? Or maybe that'll brick itself when we start removing entities from the list?
 */
 
-//Base Component struct
-struct Component
+//
+struct ComponentPointer
 {
-	int size;
-	void* address;
+	int id;			//What type of Component are we pointing to? StatusComponent, PlayerComponent, EnemyComponent, etc..
+	void* address;	//Where is the Component struct located in memory?
+	int size;		//What's the size of the Component struct?
 };
 
-//All components go under here
+template<typename T>
+T& GetComponentValue(const ComponentPointer& comp)
+{
+	//Returns the actual specific Component stored at the address that the ComponentPointer is pointing at
+	//Template magic courtesy of Elliot
+	T* t = (T*)comp.address;
+
+	//Dereference to get the value
+	return *t;
+
+	//Danger-zone
+	//if (sizeof(T) == comp.size)
+	//{
+	//	//Convert void* to correct pointer type
+	//	t = (T*)comp.address;
+
+	//	//Dereference to get the value
+	//	return *t;
+	//}
+	//else inout = T{0}; //Have some sort of error message here x
+}
+
+//All components go under here (hate the current "type" system, but I need some identifier letting us know what Component we're working with, which gets weird with templates)
 struct StatusComponent
 {
 	int hp;
@@ -30,6 +53,38 @@ struct StatusComponent
 	StatusComponent(int n) : hp(n) {}
 };
 
+struct GraphicsComponent
+{
+	//posX, posY, texture..
+};
+
+struct PhysicsComponent
+{
+	float mass;
+	float lastPosX;
+	float lastPosY;
+};
+
+struct CircleColliderComponent
+{
+	float radius, offsetX, offsetY;
+};
+
+struct ConvexColliderComponent
+{
+	float centerX, centerY;
+
+	int cornerAmount;
+	float cornerX[16], cornerY[16];
+};
+
+struct GeometryIndependentColliderComponent
+{
+	uint8_t pixelMap[512 * 512]; //#define 512*512 as size? or rather just 512 as dimension
+	float offsetX, offsetY;
+};
+
+
 struct PlayerComponent
 {
 	std::string name;
@@ -37,6 +92,14 @@ struct PlayerComponent
 	PlayerComponent() = default;
 	PlayerComponent(std::string s) : name(s) {}
 };
+
+template <typename T>
+int GetComponentID()
+{
+	//int id = &(GetComponentID<T>);
+	//Convert function to function pointer, convert that to void pointer, then take its value (adress) into an int
+	return (int)((void*)&(GetComponentID<T>));
+}
 
 struct EnemyComponent
 {
@@ -51,9 +114,30 @@ struct EnemyComponent
 struct Entity
 {
 	int index;
-	std::vector<Component> components;
 
-	Entity(int idx, std::vector<Component> vec) : index(idx), components(vec) {}
+	std::vector<ComponentPointer> components;
+
+	Entity(int idx, std::vector<ComponentPointer> vec) : index(idx), components(vec) {}
+
+	template <typename T>
+	bool HasComponent(T& comp)
+	{
+		for (const auto& component : components)
+		{
+			if (component.id == GetComponentID<T>())
+				return true;
+		}
+		return false;
+	}
+};
+
+struct View
+{
+	std::vector<Entity> entities;
+
+	View(std::vector<Entity> in) : entities(in) {}
+
+	size_t size() { return entities.size(); }
 };
 
 //Container for all our entities
@@ -66,50 +150,47 @@ struct Registry
 
 	Entity CreateEntity()
 	{
-		std::vector<Component> comps;
+		std::vector<ComponentPointer> comps;
 
 		Entity entity = Entity(index++, comps);
 		entities.push_back(entity);
 
 		return entity; //Return the entity as we create it so we can easily add components to this specific entity
 	}
-};
 
-//Encapsulate to be more clear if we use it across several text-files? idk might be unnecessary
-namespace EntityLib
-{
+	size_t size() { return entities.size(); }
+
 	template<typename T>
-	void AddComponent(Registry& registry, Entity& entity, T& toCreate)
+	void AddComponent(Entity& entity, T& toCreate)
 	{
 		//Convert any type of component (PlayerComponent, EnemyComponent, StatusComponent etc..) into the base Component struct (pointer tbh)
-		Component comp;
+		ComponentPointer comp;
 		comp.address = &toCreate;
 		comp.size = sizeof(T);
+		comp.id = GetComponentID<T>();
 
 		//Add component to entity
 		entity.components.push_back(comp);
 
 		//Add to registry vector so we can iterate
-		registry.entities.at(entity.index).components.push_back(comp);
+		entities.at(entity.index).components.push_back(comp);
 	}
 
-	template<typename T>
-	T& GetComponentValue(const Component& comp)
+	template <typename T, typename ...Args>
+	View CreateView(Args ...args)
 	{
-		//Get the value stored at the address that the base Component struct is pointing at
-		//Template compiler magic courtesy of Elliot
-		T* t;
-		if (sizeof(T) == comp.size)
+		std::vector<Entity> toReturn;
+		T comp;
+		for (auto& entity : entities)
 		{
-			//Convert void* to correct pointer type
-			t = (T*)comp.address;
-
-			//Dereference to get the value
-			return *t;
+			if (entity.HasComponent(comp))
+			{
+				toReturn.push_back(entity);
+			}
 		}
-		//else inout = T{0}; //Have some sort of error message here x
+		return View(toReturn);
 	}
-}
+};
 
 /*
 Currently we have to hard-code components, I want to change the way this works but for now: It is what it is
@@ -121,4 +202,6 @@ Step 3: Add component(s) to the entity								-->	EntityLib::AddComponent(regist
 
 If you want to get the value stored in this component (ehe)			-->	Iterate through registry.entities and do 
 																		EntityLib::GetComponentValue<ComponentType>(entity.componentThatYouWantToGetValueOf)
+
+If you want to get a view of only entities with specific components	-->	auto view = registry.CreateView<StatusComponent>()
 */
