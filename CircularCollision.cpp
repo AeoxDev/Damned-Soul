2d70c13f3877bb39488fb640f904e2c35388f4a3
Felix Mathiasson <felix.mathiasson99@gmail.com>
2023-09-07 11:04:30 +0200

    commented out pseudo code

--- CircularCollision.cpp
+++ CircularCollision.cpp
@@ -4,12 +4,13 @@ bool IsCircularCollision(const Entity& entity1, const Entity& entity2)
 {
 	// Needs to loop through entities in case of several hitboxes
 
-	float dx = entity1.components[CIRCLE_COMPONENT_INDEX].posX - entity2.components[CIRCLE_COMPONENT_INDEX].posX;
+	/*float dx = entity1.components[CIRCLE_COMPONENT_INDEX].posX - entity2.components[CIRCLE_COMPONENT_INDEX].posX;
 	float dy = entity1.components[CIRCLE_COMPONENT_INDEX].posY - entity2.components[CIRCLE_COMPONENT_INDEX].posY;
 
 	float distance = std::sqrt(dx * dx + dy * dy);
 
-	return distance <= (entity1.radius + entity2.radius);
+	return distance <= (entity1.radius + entity2.radius);*/
+	return false;
 }
 
 void CollisionEvent(Entity& entity, Entity& entity2)
