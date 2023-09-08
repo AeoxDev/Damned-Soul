70f6a523c67a75b7a94a37e315db0dc7bc1ce474
Elliot Lundin <firetail4373@gmail.com>
2023-09-08 11:04:52 +0200

    Added preparatory files for convex and geometry independent collision.

---
+++ header/GeometryIndependent.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void CreateGeometryIndependentCollision(/*StageEntity*/);
---
+++ source/GeometryIndependent.cpp
@@ -0,0 +1,17 @@
+#include "GeometryIndependent.h"
+#include "Camera.h"
+
+struct GeometryIndependent
+{
+	//TextureComponent
+	float offsetX, offsetY;
+};
+
+void CreateGeometryIndependentCollision(/*StageEntity*/)
+{
+	//Find stage entity
+	//Look at model using orthographic camera
+	//Save to a texture with custom size (int8 per pixel)
+	//Add this component to stage entity
+	//Return.
+}
