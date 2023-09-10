ff0d19175bc690ad24d81599802aab4e7d5219c9
Exoro123 <matte.nordin@live.se>
2023-09-10 17:03:24 +0200

    WIP ui graphics

--- header/D2D1Graphics.h
+++ header/D2D1Graphics.h
@@ -1,4 +1,26 @@
 #pragma once
 #include <dxgi.h>
+#include "D3D11Helper.h"
 
 extern IDXGISurface* UISurface;
+/*
+class D2D1Graphics
+{
+private:
+	struct RenderSetupComponent
+	{
+		PS_IDX	UIPixelShader;
+		VS_IDX	UIVertexShader;
+		VB_IDX	UIVertexBuffer;
+		IB_IDX	UIIndexBuffer;
+		RTV_IDX UIrtv;
+		DSV_IDX UIdsv;
+	};
+
+public:
+	D2D1Graphics();
+	~D2D1Graphics();
+
+	IDXGISurface* GetSurface();
+};
+*/
