0bda70460c10e7615fac11b54d7a13ee2b9de993
Elliot Lundin <firetail4373@gmail.com>
2023-09-14 10:25:23 +0200

    Added function for convex to convex collision using line to line intersection math.

--- source/ConvexCollision.cpp
+++ source/ConvexCollision.cpp
@@ -126,8 +126,135 @@ bool IsConvexCircularCollision(Registry& registry, EntityID& entity1, EntityID& 
 //	}
 //}
 
-bool IsConvexCollision(Registry& registry, EntityID& entity1, EntityID& entity2, int circle1, int circle2)
+struct ConvexReturnData
 {
-	//
-	return false;
+	bool hit;
+	float line1X, line1Z, line2X, line2Z;
+};
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="line1Corner1X">= l1c1x</param>
+/// <param name="line1Corner1Z">= l1c1z</param>
+/// <param name="line1Corner2X">= l1c2x</param>
+/// <param name="line1Corner2Z">= l1c2z</param>
+/// <param name="line2Corner1X">= l2c1x</param>
+/// <param name="line2Corner1Z">= l2c1z</param>
+/// <param name="line2Corner2X">= l2c2x</param>
+/// <param name="line2Corner2Z">= l2c2z</param>
+/// <returns></returns>
+ConvexReturnData LineToLineIntersection(float l1c1x, float l1c1z, float l1c2x, float l1c2z, float l2c1x, float l2c1z, float l2c2x, float l2c2z)
+{
+	//Get denominator to get where on line the intersection is from 0 to 1
+	float denominator = (l2c2x - l2c1x) * (l1c1z - l1c2z) - (l1c1x - l1c2x) * (l2c2z - l2c1z);
+
+	//Get where on the lines the intersection happens, use denominator to get to 0 to 1 if on line
+	float whereOnLine1 = ((l2c1z - l2c2z) * (l1c1x - l2c1x) + (l2c2x - l2c1x) * (l1c1z - l2c1z)) / denominator;
+	float whereOnLine2 = ((l1c1z - l1c2z) * (l1c1x - l2c1x) + (l1c2x - l1c1x) * (l1c1z - l2c1z)) / denominator;
+	ConvexReturnData toReturn = { 0 };
+	//Check if the intersection happens between the corners of the lines
+	//if outside of 0 and 1 the intersection is outside of convex shape
+	if (whereOnLine1 >= 0.0f && whereOnLine1 < 1.0f && whereOnLine2 >= 0.0f && whereOnLine2 < 1.0f)
+	{
+		toReturn.line1X = (1.0f - whereOnLine1) * (l1c2x - l1c1x);
+		toReturn.line1Z = (1.0f - whereOnLine1) * (l1c2z - l1c1z);
+		toReturn.line2X = (1.0f - whereOnLine2) * (l2c2x - l2c1x);
+		toReturn.line2Z = (1.0f - whereOnLine2) * (l2c2z - l2c1z);
+		toReturn.hit = true;
+	}
+	return toReturn;
+}
+
+bool IsConvexCollision(Registry& registry, EntityID& entity1, EntityID& entity2, int convexID1, int convexID2)
+{
+	HitboxComponent *convex1 = registry.GetComponent<HitboxComponent>(entity1);
+	HitboxComponent *convex2 = registry.GetComponent<HitboxComponent>(entity2);
+
+	// get a hold of both the circles' bit flags to check if they can collide or not
+	if (!convex1 || !convex2)
+	{
+		return false;
+	} // add more check to more flags later, try with this first to see if the math works
+
+	//Bitmask circle1 is to circle2 hit
+	unsigned short iSmask = 0b0000000001111110;// is mask
+	unsigned short hitMask = 0b0001111110000000;
+	unsigned short* r1 = (unsigned short*)&convex1->circularFlags[convexID1];
+	unsigned short* r2 = (unsigned short*)&convex2->circularFlags[convexID2];
+
+	bool iShit1 = ((*r1 & iSmask) << 6) & (*r2 & hitMask);//circle1 hit circle2. is hit
+	bool iShit2 = ((*r2 & iSmask) << 6) & (*r1 & hitMask);//circle2 hit circle1. is hit
+
+	//If none can interact return false
+	if (!iShit1 && !iShit2)
+	{
+		return false;
+	}
+
+	bool collided = false;
+	//First check if bounding radius reach each other
+	float posX = convex1->convexHitbox[convexID1].centerX;
+	float posZ = convex1->convexHitbox[convexID1].centerZ;
+	float pos2X = convex2->convexHitbox[convexID2].centerX;
+	float pos2Z = convex2->convexHitbox[convexID2].centerZ;
+
+	float dx = posX - pos2X;
+	float dz = posZ - pos2Z;
+
+	//If distance from each other is greater than radi, no circular collision, skip.
+	if (sqrtf(dx*dx + dz*dz) > convex1->convexHitbox[convexID1].boundingRadius + convex2->convexHitbox[convexID2].boundingRadius)
+	{
+		return false;
+	}
+	ConvexReturnData returnedData = {0};
+	int whichLine = 0;
+	//Now we can check which lines collide
+	for (size_t i = 0; i < convex1->convexHitbox[convexID1].cornerAmount; i++)
+	{
+		for (size_t j = 0; j < convex2->convexHitbox[convexID2].cornerAmount; j++)
+		{
+			//Check from center to corner of one convex shape onto the sides of another.
+			returnedData = LineToLineIntersection(convex1->convexHitbox[convexID1].centerX,
+				convex1->convexHitbox[convexID1].centerZ,
+				convex1->convexHitbox[convexID1].cornerX[i], convex1->convexHitbox[convexID1].cornerZ[i],
+				convex2->convexHitbox[convexID2].cornerX[j], convex2->convexHitbox[convexID2].cornerZ[j],
+				convex2->convexHitbox[convexID2].cornerX[(1 + j) % CONVEX_CORNER_LIMIT], convex2->convexHitbox[convexID2].cornerZ[(1 + j) % CONVEX_CORNER_LIMIT]);
+			if (returnedData.hit)
+			{	
+				whichLine = j;
+				//If we add convex hurtboxes for enemies we can add moving them correctly here.
+				break;
+			}
+		}
+	}
+	OnCollisionParameters params = {};
+	params.registry = registry;//Reggie stiel
+
+	if (iShit1)
+	{
+		params.entity1 = entity1;
+		params.entity2 = entity2;
+		params.hitboxID1 = convexID1;
+		params.hitboxID2 = convexID2;
+		params.normal1X = -convex2->convexHitbox[convexID2].normalX[whichLine];
+		params.normal1Z = -convex2->convexHitbox[convexID2].normalZ[whichLine];
+		params.normal2X = convex2->convexHitbox[convexID2].normalX[whichLine];
+		params.normal2Z = convex2->convexHitbox[convexID2].normalZ[whichLine];
+		convex1->onCircleCollision[convexID1].CollisionFunction(params);
+	}
+	if (iShit2)
+	{
+		params.entity1 = entity2;
+		params.entity2 = entity1;
+		params.hitboxID1 = convexID1;
+		params.hitboxID2 = convexID2;
+		params.normal1X = convex2->convexHitbox[convexID2].normalX[whichLine];
+		params.normal1Z = convex2->convexHitbox[convexID2].normalZ[whichLine];
+		params.normal2X = -convex2->convexHitbox[convexID2].normalX[whichLine];
+		params.normal2Z = -convex2->convexHitbox[convexID2].normalZ[whichLine];
+		convex2->onCircleCollision[convexID2].CollisionFunction(params);
+	}
+
+	return returnedData.hit;
 }
