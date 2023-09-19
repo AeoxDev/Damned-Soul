281cb5ef1aada316463302b1a886431849741027
Felix Mathiasson <felix.mathiasson99@gmail.com>
2023-09-19 10:51:02 +0200

    added comments for clarity to lighting.h

--- header/Lighting.h
+++ header/Lighting.h
@@ -5,13 +5,13 @@
 
 struct LightingStruct
 {
-	DirectX::XMFLOAT4	ambientColor;
-	DirectX::XMFLOAT4	diffuseColor;
-	DirectX::XMFLOAT4	specularColor;
-	float intensity;
+	DirectX::XMFLOAT4	ambientColor; // Kind of like background lighting
+	DirectX::XMFLOAT4	diffuseColor; // Color of the light
+	DirectX::XMFLOAT4	specularColor; // reflection, shiny 
+	float intensity; // focus of the light. Basically tigher/focused vs broader/stofter
 };
 
 
-bool CreateLightingConstantBuffer(ID3D11Device* device, ID3D11Buffer** constantBuffer);
+bool CreateLightingConstantBuffer(ID3D11Device* device, ID3D11Buffer** constantBuffer); //create a lighting constant buffer
 
-void UpdateLightingBuffer(ID3D11DeviceContext* context, ID3D11Buffer* lightingConstantBuffer, LightingStruct& lightingData);
+void UpdateLightingBuffer(ID3D11DeviceContext* context, ID3D11Buffer* lightingConstantBuffer, LightingStruct& lightingData); // update the values of the constant buffer
