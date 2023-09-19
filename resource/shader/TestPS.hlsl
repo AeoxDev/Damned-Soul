c329ca5fc091e20f909eaeb2e3a3c66a0b5f0355
Felix Mathiasson <felix.mathiasson99@gmail.com>
2023-09-19 11:24:35 +0200

    added a cbuffer in testPS.hlsl

--- resource/shader/TestPS.hlsl
+++ resource/shader/TestPS.hlsl
@@ -4,6 +4,16 @@ Texture2D emissionTex : register(t2);
 
 SamplerState WrapSampler : register(s0);
 
+
+cbuffer LightingBuffer : register(b0)
+{
+    float3 ambient;
+    float3 diffuse;
+    float3 specular;
+    float intensity;
+}
+
+
 struct VS_OUT
 {
 	float4 position : SV_POSITION;
@@ -11,6 +21,7 @@ struct VS_OUT
 	uint material : MATERIAL;
 	float2 uv : UV;
 };
+
 
 
 
