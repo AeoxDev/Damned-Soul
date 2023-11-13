02d5612717e0e043b9c1160a4571f9441680e594
Elliot Lundin <firetail4373@gmail.com>
2023-11-13 15:08:17 +0100

    Fixed bug with player hitbox staying active when dashing.

--- source/Skynet/Projectile.cpp
+++ source/Skynet/Projectile.cpp
@@ -4,42 +4,48 @@
 
 void CreateProjectile(EntityID entity, float directionX, float directionZ)
 {
-	EntityID bullet = registry.CreateEntity();
+	int nrProjectiles = 2 + rand() % 4;
+	for (size_t i = 0; i < nrProjectiles; i++)
+	{
+		EntityID bullet = registry.CreateEntity();
 
-	//Model of the bullet
-	registry.AddComponent<ModelBonelessComponent>(bullet, LoadModel("AcidPlaceholder.mdl"));
+		//Model of the bullet
+		registry.AddComponent<ModelBonelessComponent>(bullet, LoadModel("AcidPlaceholder.mdl"));
 
-	TransformComponent* origin = registry.GetComponent<TransformComponent>(entity);
+		TransformComponent* origin = registry.GetComponent<TransformComponent>(entity);
 
-	//Transform
-	TransformComponent transform;
-	transform.mass = 1.0f;
-	//Direction
-	transform.facingX = directionX; 
-	transform.facingY = 0;
-	transform.facingZ = directionZ;
+		//Transform
+		TransformComponent transform;
+		transform.mass = 1.0f;
+		//Direction
+		transform.facingX = directionX + (float)(rand() % 8) * 0.1f;
+		transform.facingY = 0;
+		transform.facingZ = directionZ + (float)(rand() % 8) * 0.1f;
+
+		//Position
+		transform.positionX = origin->positionX;
+		transform.positionY = 1.0f;
+		transform.positionZ = origin->positionZ;
+
+		//Scale
+		float scaleFactor = 6.0f;
+		transform.scaleX = scaleFactor;
+		transform.scaleY = scaleFactor;
+		transform.scaleZ = scaleFactor;
+
+		registry.AddComponent<TransformComponent>(bullet, transform);
+
+		registry.AddComponent<ProjectileComponent>(bullet, 0, entity);
+
+		//Stats needed in order to deal damage
+		//Health, Speed, Damage, attackSpeed
+		StatComponent* stats = registry.AddComponent<StatComponent>(bullet, 1.0f, 25.0f, 15.0f, 0.0f);
+		stats->UpdateBonusKnockback(2.0f);
+		stats->baseHazardModifier = 0.0f;
+		stats->hazardModifier = 0.0f;
+
+		//Setup Bullet hitbox
+		SetupProjectileCollisionBox(bullet, 1.0f);
+	}
 	
-	//Position
-	transform.positionX = origin->positionX; 
-	transform.positionY = 1.0f; 
-	transform.positionZ = origin->positionZ;
-	
-	//Scale
-	float scaleFactor = 6.0f;
-	transform.scaleX = scaleFactor; 
-	transform.scaleY = scaleFactor; 
-	transform.scaleZ = scaleFactor;
-
-	registry.AddComponent<TransformComponent>(bullet, transform);
-
-	registry.AddComponent<ProjectileComponent>(bullet, 0, entity);
-
-	//Stats needed in order to deal damage
-	//Health, Speed, Damage, attackSpeed
-	StatComponent* stats = registry.AddComponent<StatComponent>(bullet, 1.0f, 30.0f, 15.0f, 0.0f);
-	stats->baseHazardModifier = 0.0f;
-	stats->hazardModifier = 0.0f;
-	
-	//Setup Bullet hitbox
-	SetupProjectileCollisionBox(bullet, 1.0f);
 }
