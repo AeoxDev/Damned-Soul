31d10f328add13ba2ad0ecb56df39fee6e4a3dc8
Exoro123 <71072627+Exoro123@users.noreply.github.com>
2023-11-16 14:08:14 +0100

    added new eye model and anims

--- source/Level/LevelHelper.cpp
+++ source/Level/LevelHelper.cpp
@@ -269,7 +269,7 @@ EntityID SetupEnemy(EnemyType eType, float positionX , float positionY , float p
 		stat->baseHazardModifier = 0.0f;
 		stat->baseCanWalkOnCrack = true;
 		stat->canWalkOnCrack = true;
-		model = registry.AddComponent<ModelSkeletonComponent>(entity, LoadModel("EyePlaceholder.mdl"));
+		model = registry.AddComponent<ModelSkeletonComponent>(entity, LoadModel("Eye.mdl"));
 		registry.AddComponent<AnimationComponent>(entity);
 		registry.AddComponent<EyeBehaviour>(entity);
 		SetupEnemyCollisionBox(entity, 1.f, EnemyType::eye, false);
