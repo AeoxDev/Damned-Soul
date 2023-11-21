6a4b52dd0b51a58aac1b452c72a2a9a9c4272fd5
Dripes03 <eriksvensso1@gmail.com>
2023-11-21 10:05:47 +0100

    Modified blend animations component to handle two animations

--- header/Components/BlendAnimationsComponent.h
+++ header/Components/BlendAnimationsComponent.h
@@ -3,12 +3,18 @@
 #include "AnimationType.h"
 #include <stdint.h>
 
-#define BLEND_ANIM_BRANCHLESS(comp) (comp->aAnimTime -= (float)(1 < comp->aAnimTime) * int(comp->aAnimTime))
+#define BLEND_ANIM1_BRANCHLESS(comp) (comp->aAnimTime1 -= (float)(1 < comp->aAnimTime1) * int(comp->aAnimTime1))
+#define BLEND_ANIM2_BRANCHLESS(comp) (comp->aAnimTime2 -= (float)(1 < comp->aAnimTime2) * int(comp->aAnimTime2))
 
 struct BlendAnimationComponent
 {
-	ANIMATION_TYPE aAnim = ANIMATION_IDLE;
-	uint8_t aAnimIdx = 0;
-	float aAnimTime = 0;
-	float aAnimTimeFactor = 1.0f;
+	ANIMATION_TYPE aAnim1 = ANIMATION_IDLE;
+	uint8_t aAnimIdx1 = 0;
+	float aAnimTime1 = 0;
+	float aAnimTimeFactor1 = 1.0f;
+	
+	ANIMATION_TYPE aAnim2 = ANIMATION_IDLE;
+	uint8_t aAnimIdx2 = 0;
+	float aAnimTime2 = 0;
+	float aAnimTimeFactor2 = 1.0f;
 };
