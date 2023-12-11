7a24d0595b995ce54f39f79581cb9d6a3a46a1f6
Dripes03 <eriksvensso1@gmail.com>
2023-12-11 11:01:04 +0100

    comments

--- source/Skynet/ZacBehaviourSystem.cpp
+++ source/Skynet/ZacBehaviourSystem.cpp
@@ -58,7 +58,7 @@ bool ZacBehaviourSystem::Update()
 				zacTransformComponent->positionX += dirX * GetDeltaTime() * enemyStats->GetSpeed();
 				zacTransformComponent->positionZ += dirZ * GetDeltaTime()* enemyStats->GetSpeed();
 
-				if (zacComponent->zacIndex == 1) //time to animate
+				if (zacComponent->zacIndex == 1) //Leg animation
 				{
 					enemyAnim->aAnim = ANIMATION_WALK;
 					enemyAnim->aAnimIdx = 0;
@@ -66,7 +66,7 @@ bool ZacBehaviourSystem::Update()
 					ANIM_BRANCHLESS(enemyAnim);
 					enemyAnim->aAnimTime += GetDeltaTime() * enemyAnim->aAnimTimeFactor;
 				}
-				else
+				else //Other animations
 				{
 					enemyAnim->aAnim = ANIMATION_WALK;
 					enemyAnim->aAnimIdx = 0;
