b24d9d782b5cf69e23d340b47e2f42b5cb55cf2b
Arwat <araa18@student.bth.se>
2023-12-14 15:55:21 +0100

    Fixed visuals of boss slam particles

--- header/Relics/Utility/RelicParticleHelper.h
+++ header/Relics/Utility/RelicParticleHelper.h
@@ -2,7 +2,6 @@
 #include "EntityID.h"
 
 void ParticleAtEntityLocation(const EntityID& origin, const float duration, void* funcStart);
-void ParticleAtEntityLocationFollow(const EntityID& origin, const float duration, void* funcStart);
-
 void ParticleAtEntityFollow(const EntityID& origin, const float duration, void* funcStart);
+
 void _CheckLiving(const EntityID& child, const int& index);
